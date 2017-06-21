bd133adcaa33d7e2e4fab7e41bc852536f30d9f8
Jeroen <jeroenooms@gmail.com>
2017-06-21 10:31:47 +0200

    Simpler unit test

--- tests/testthat/test-encoding.R
+++ tests/testthat/test-encoding.R
@@ -11,12 +11,19 @@ test_that("String encodings", {
   )
   obj_utf8 <- structure(as.list(enc2utf8(strings)), names = letters[seq_along(strings)])
 
-  # Test all UTF-8 strings
-  expect_equal(ocpu_post_multipart('/library/base/R/list', obj_utf8), obj_utf8)
-  expect_equal(ocpu_post_encoded('/library/base/R/list', obj_utf8), obj_utf8)
-  expect_equal(ocpu_post_json('/library/base/R/list', obj_utf8), obj_utf8)
-  expect_equal(ocpu_post_rds('/library/base/R/list', obj_utf8), obj_utf8)
-  expect_equal(ocpu_post_pb('/library/base/R/list', obj_utf8), obj_utf8)
+  # Roundtrip string objects
+  expect_equal(ocpu_post_multipart('/library/base/R/identity', list(x = obj_utf8)), obj_utf8)
+  expect_equal(ocpu_post_encoded('/library/base/R/identity', list(x = obj_utf8)), obj_utf8)
+  expect_equal(ocpu_post_json('/library/base/R/identity', list(x = obj_utf8)), obj_utf8)
+  expect_equal(ocpu_post_rds('/library/base/R/identity', list(x = obj_utf8)), obj_utf8)
+  expect_equal(ocpu_post_pb('/library/base/R/identity', list(x = obj_utf8)), obj_utf8)
+
+  # Strings as arguments (expressions)
+  # expect_equal(ocpu_post_multipart('/library/base/R/list', obj_utf8), obj_utf8)
+  # expect_equal(ocpu_post_encoded('/library/base/R/list', obj_utf8), obj_utf8)
+  # expect_equal(ocpu_post_json('/library/base/R/list', obj_utf8), obj_utf8)
+  # expect_equal(ocpu_post_rds('/library/base/R/list', obj_utf8), obj_utf8)
+  # expect_equal(ocpu_post_pb('/library/base/R/list', obj_utf8), obj_utf8)
 
   # Test all native strings (usualy only on Windows)
   #obj_native <- structure(as.list(enc2native(strings)), names = letters[seq_along(strings)])
